5eb1e6221dea76e608702cd68897423dcf483ca7
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-03 00:45:24 -0700

    Refactor: fix #Cells test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,11 +3,14 @@ require './lib/cell'
 require './lib/board'
 
 RSpec.describe Board do
-    describe '#Initialize' do
-        board = Board.new
+    describe '#Cells' do
+        it 'Initializes, creates cells and stores it on a board' do
+            board = Board.new.cells
         
-        expect(board.cells).to be Hash
-        expect(board.cells.size).to eq(7)
-        expect(board.cells).to eq({'A' => ['.','.','.','.','.','.']})
+            expect(board.class).to be Hash
+            expect(board.size).to eq(7)
+            expect(board.keys).to eq(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+            expect(board.values.flatten).to all(be_an_instance_of Cell)
+        end
     end
 end
