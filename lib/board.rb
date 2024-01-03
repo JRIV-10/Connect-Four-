59e5a5e2d2384518a021fde72acace921aa97376
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-03 14:51:45 -0700

    Feat: create draft for place_piece method for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,11 @@
 class Board
-
-    def self.cells
-        board = Hash.new([])
+    
+    def initialize
+        board = Hash.new
+    end
+    
+    def cells
+        board = Hash.new
         keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
         keys.map do |key|
             values = []
@@ -13,5 +17,25 @@ class Board
             board[key] = values
         end
         board
+    end
+
+    def available_cell(column)
+        # @@board[column].find_last do |cell|
+        #     cell.empty?
+        #     require 'pry'; binding.pry
+        # end
+        column_cells = cells[column]
+        column_cells.reverse.find do |cell|
+            cell.empty?
+        end
+    end
+
+    def place_piece(column,value)
+        first_av_cell = available_cell(column)
+        cells[column].find do |cell|
+            if cell == first_av_cell
+                cell.assign_value(value)
+            end
+        end
     end
 end
