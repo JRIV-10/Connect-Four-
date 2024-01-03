b866703d0aece911b34da9a456b1f0c4ae3233c7
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-03 00:45:46 -0700

    Refactor: fix #Cells method for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,17 @@
 class Board
 
-    def Initialize
-        Hash.new([])
-    end
-
     def cells
-        board = {}
-        
+        board = Hash.new([])
+        keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        keys.map do |key|
+            values = []
+            index = 1
+            6.times do 
+                values << Cell.new(key , index)
+                index += 1
+            end
+            board[key] = values
+        end
+        board
     end
 end
