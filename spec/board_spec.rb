2df97816efb70149ba156e3fcd5515a784db9203
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-03 13:33:07 -0700

    Test: Create initializes with all empty cells test for Board class, and refactor to cells method as class method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,12 +5,23 @@ require './lib/board'
 RSpec.describe Board do
     describe '#Cells' do
         it 'Initializes, creates cells and stores it on a board' do
-            board = Board.new.cells
+            board = Board.cells
         
             expect(board.class).to be Hash
             expect(board.size).to eq(7)
             expect(board.keys).to eq(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
             expect(board.values.flatten).to all(be_an_instance_of Cell)
+        end
+
+        it 'Initializes with all empty cells' do
+            board = Board.cells
+            # require 'pry'; binding.pry
+            
+            expected = board.values.flatten.all? do |cell| 
+                cell.empty?
+            end
+
+            expect(expected).to be true
         end
     end
 end
