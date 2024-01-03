20b7b83d920ddd3b53c0de0bf27d2627c0f2cf2a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-03 14:51:12 -0700

    Test: create test for place_piece method for Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,8 +4,9 @@ require './lib/board'
 
 RSpec.describe Board do
     describe '#Cells' do
+        
         it 'Initializes, creates cells and stores it on a board' do
-            board = Board.cells
+            board = Board.new.cells
         
             expect(board.class).to be Hash
             expect(board.size).to eq(7)
@@ -14,7 +15,7 @@ RSpec.describe Board do
         end
 
         it 'Initializes with all empty cells' do
-            board = Board.cells
+            board = Board.new.cells
             # require 'pry'; binding.pry
             
             expected = board.values.flatten.all? do |cell| 
@@ -22,6 +23,20 @@ RSpec.describe Board do
             end
 
             expect(expected).to be true
+        end
+    end
+
+    describe '#place_piece' do
+        
+        it 'Iterates to find first open available cell in a column' do
+            board = Board.new
+            #require 'pry'; binding.pry
+            # turn_column = 'A'
+            # turn_value = 'X'
+            # board_result = board.place_piece('A','X')
+            new_board = board.place_piece('A','X')
+        
+            expect(new_board.values).to eq(['.','.','.','.','.','X'])
         end
     end
 end
