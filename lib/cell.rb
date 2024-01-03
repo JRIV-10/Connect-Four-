72fcab6ca7e13ed652076bf737e5ad8ad2a2746c
Joey Reyes <joeyreyes105@gmail.com>
2024-01-02 20:01:11 -0800

    Add empty? and assgin vaule methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,19 @@
 class Cell 
     attr_reader :column,
-                :row 
+                :row, 
+                :value
 
     def initialize(column, row)
         @column = column
         @row = row 
+        @value = '.'
     end 
+
+    def empty?
+        @value == '.'
+    end
+
+    def assign_value(value)
+        @value = value
+    end
 end 
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -7,6 +7,27 @@ RSpec.describe Cell do
             
             expect(cell.column).to eq('A')
             expect(cell.row).to eq(6)
+            expect(cell.value).to eq('.')
         end 
     end 
+
+    describe '#empty?' do 
+        it 'is empty when initialized' do 
+            cell = Cell.new('A', 6)
+
+            expect(cell.empty?).to eq(true) 
+        end
+    end
+
+    describe '#assign_value' do 
+        it 'can store a value' do 
+            cell = Cell.new('A', 6)
+            
+            expect(cell.empty?).to eq(true)
+
+            cell.assign_value('X') 
+
+            expect(cell.empty?).to eq(false)
+        end
+    end
 end 
