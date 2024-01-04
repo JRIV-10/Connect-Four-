a4c31d8f5197e5fe61eebfa9d7b4f8d47e2a623c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-04 16:44:42 -0700

    Feat: Create method and helper methods for process_main_menu_input on StartGame class

--- lib/start_game.rb
+++ lib/start_game.rb
@@ -1,4 +1,5 @@
 class StartGame 
+    attr_reader :board
 
     def give_welcome
        puts 'Welcome to CONNECT FOUR\n Enter p to play. Enter q to quit.'
@@ -8,44 +9,22 @@ class StartGame
 
     def process_main_menu_input(input)
         if  input == 'p'
-            board = Board.new
-            board.return_board(board)
+            @board = Board.new
+            return_turn_messaje
+            return_matrix(@board)
             "Let's play the game"
         elsif input == 'q'
             'See you next time, goodbye'
         else
             'Invalid placement. Please enter p or q'
         end
-    end
-    
-    def return_board(board)
-        puts 'Please Enter your column selection: letter A to G'
-        puts ' ------------------------------------------------ '
-        
-        formatted_board = format_board(board)
-        board.render(formatted_board).join('\n')
-    end
-    
-    def render(board)    
-        matrix = Array.new(6) { Array.new(7, '') }
-        columns = board.keys
-        columns.each_with_index do |key, index|
-            column_cells = board[key]
-            column_cells.each do |cell|
-                matrix[cell.row - 1][index] = cell.value
-            end
-        end
-        matrix.unshift(columns)
-
-        result_matrix = matrix.map { |row| row.join }
+    end    
+    def return_turn_messaje
+        'Please Enter your column selection: letter A to G/n Or feel free to Enter q to quit the game\n  ------------------------------------------------'
     end
 
-    def format_board(board)
-        if board.class == Board
-            board.board
-        elsif board.class == Hash
-            board 
-        end
+    def return_matrix(board)
+        board.return_board
     end
 
 end
