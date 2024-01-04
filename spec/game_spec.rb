597e4adbf8f315d316a2bc2b277df512f878b363
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-04 14:56:50 -0700

    Test: Draft initial tests for Game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+require './spec/spec_helper'
+
+RSpec.describe Game do
+    describe '#Initialize' do
+        it 'Exists' do
+            game = Game.new
+
+            expect(game).to be_a Game
+        end
+
+        it '' do
+            
+        end
+    end
+end
